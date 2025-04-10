c01d5db7194bae816ec19ff71cb03c7155150013
Robert Huddleston <robert@avodagroup.us>
2025-04-10 13:31:30 -0400

    Updates

--- src/main/java/com/avodagroup/connectors/pdfBox/internal/PdfBoxConfiguration.java
+++ src/main/java/com/avodagroup/connectors/pdfBox/internal/PdfBoxConfiguration.java
@@ -7,21 +7,11 @@
 package com.avodagroup.connectors.pdfBox.internal;
 
 import org.mule.runtime.extension.api.annotation.Operations;
-import org.mule.runtime.extension.api.annotation.connectivity.ConnectionProviders;
-import org.mule.runtime.extension.api.annotation.param.Parameter;
 
 /**
  * This class represents an extension configuration, values set in this class are commonly used across multiple
  * operations since they represent something core from the extension.
  */
 @Operations(PdfBoxOperations.class)
-@ConnectionProviders(PdfBoxConnectionProvider.class)
 public class PdfBoxConfiguration {
-
-  @Parameter
-  private String configId;
-
-  public String getConfigId(){
-    return configId;
-  }
 }
